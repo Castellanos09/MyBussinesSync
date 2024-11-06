45ef3d23163c4f4d0f6681e7e18fe1718f382a1b
Castellanos Imelda <imelda.castellanos09@gmail.com>
2024-11-06 01:09:25 -0600

    CÓDIGO PARA CERRAR UNA SESIÓN

--- app/src/main/java/garcia/imelda/mybussinessync_kotlin_jc/ViewModels/NotasViewModel.kt
+++ app/src/main/java/garcia/imelda/mybussinessync_kotlin_jc/ViewModels/NotasViewModel.kt
@@ -1,6 +1,15 @@
 package garcia.imelda.mybussinessync_kotlin_jc.ViewModels
 
 import androidx.lifecycle.ViewModel
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class NotasViewModel : ViewModel(){
+    private val auth : FirebaseAuth = Firebase.auth
+
+    fun logout() {
+        auth.signOut() //CERRAR SESIÓN
+    }
+
 }
--- app/src/main/java/garcia/imelda/mybussinessync_kotlin_jc/Views/Notas/HomeView.kt
+++ app/src/main/java/garcia/imelda/mybussinessync_kotlin_jc/Views/Notas/HomeView.kt
@@ -1,11 +1,45 @@
 package garcia.imelda.mybussinessync_kotlin_jc.Views.Notas
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ExitToApp
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import garcia.imelda.mybussinessync_kotlin_jc.ViewModels.NotasViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeView(NavController: NavHostController, notasVM: NotasViewModel) {
-    Text(text = "Inicio")
+    Scaffold (
+        topBar = {
+            TopAppBar(title = { Text(text = "Inicio")},
+                actions = {
+                        IconButton(onClick = {
+                            notasVM.logout()
+                            NavController.popBackStack()
+                        }) {
+                            Icon(
+                                imageVector =   Icons.AutoMirrored.Filled.ExitToApp,
+                                contentDescription = "")
+                        }
+                })
+        }
+
+    ) {padding ->
+        Column(modifier = Modifier.padding(padding),
+            horizontalAlignment = Alignment.CenterHorizontally){
+
+
+
+        }
+    }
 }
