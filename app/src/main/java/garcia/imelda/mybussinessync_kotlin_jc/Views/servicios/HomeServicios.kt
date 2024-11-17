dec2fa83fce5bc05ad242c142dc42f486871fb33
Castellanos Imelda <imelda.castellanos09@gmail.com>
2024-11-16 22:28:54 -0600

    SE AGREGA COLUMNA PARA MOSTRAR CAMPOS DE LA BD

--- app/src/main/java/garcia/imelda/mybussinessync_kotlin_jc/Views/servicios/HomeServicios.kt
+++ app/src/main/java/garcia/imelda/mybussinessync_kotlin_jc/Views/servicios/HomeServicios.kt
@@ -3,6 +3,8 @@ package garcia.imelda.mybussinessync_kotlin_jc.Views.servicios
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ExitToApp
 import androidx.compose.material.icons.filled.Create
@@ -16,21 +18,27 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavController
 import garcia.imelda.mybussinessync_kotlin_jc.R
 import garcia.imelda.mybussinessync_kotlin_jc.ViewModels.NotasViewModel
 import garcia.imelda.mybussinessync_kotlin_jc.ViewModels.ServiciosViewModel
 
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeServicios(navController: NavController, serviciosVM: ServiciosViewModel) {
+    
+   LaunchedEffect(Unit) {
+      serviciosVM.getServices()
+   }
+
     Scaffold (
         topBar = {
             TopAppBar(
@@ -42,7 +50,6 @@ fun HomeServicios(navController: NavController, serviciosVM: ServiciosViewModel)
                 title = { Text(text = "Inicio")},
                 actions = {
                     IconButton(onClick = {
-                        serviciosVM.logout()
                         navController.popBackStack()
                     }) {
                         Icon(
@@ -55,6 +62,20 @@ fun HomeServicios(navController: NavController, serviciosVM: ServiciosViewModel)
     ) {padding ->
         Column(modifier = Modifier.padding(padding),
             horizontalAlignment = Alignment.CenterHorizontally){
+
+            val servicio = serviciosVM.serviceData
+
+            LazyColumn {
+                items(servicio){ item ->
+                    Text(text = item.cliente)
+                    Text(text = item.color)
+                    Text(text = item.numero)
+                    Text(text = item.presupuesto.toString())
+                    Text(text = item.servicio)
+                    Text(text = item.vehiculo)
+                }
+            }
+
 
             Button(onClick = {
                 navController.navigate("AddServicio")
@@ -72,24 +93,6 @@ fun HomeServicios(navController: NavController, serviciosVM: ServiciosViewModel)
             ) {
                 Text(text = "Agregar servicio")
             }
-
-            Button(onClick = {
-                navController.navigate("AllServices")
-            },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(start = 100.dp, end = 100.dp),
-                colors = ButtonColors(
-                    containerColor = colorResource(id = R.color.azulFuerte),
-                    contentColor = Color.White,
-                    disabledContainerColor = colorResource(id = R.color.azulBajito),
-                    disabledContentColor = Color.White
-                )
-
-            ) {
-                Text(text = "Todos los servicios")
-            }
-
         }
     }
 }
