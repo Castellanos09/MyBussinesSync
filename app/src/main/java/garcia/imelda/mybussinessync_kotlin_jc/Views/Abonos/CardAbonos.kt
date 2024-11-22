6f3279e654dad5209dcc91b6be69af5f96f0e5b2
Castellanos Imelda <imelda.castellanos09@gmail.com>
2024-11-21 22:05:53 -0600

    SE CREA CASCARON DE CARDVIEW DE ABONOS

---
+++ app/src/main/java/garcia/imelda/mybussinessync_kotlin_jc/Views/Abonos/CardAbonos.kt
@@ -0,0 +1,60 @@
+package garcia.imelda.mybussinessync_kotlin_jc.Views.Abonos
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import garcia.imelda.mybussinessync_kotlin_jc.R
+
+@Composable
+fun CardAbono(
+    descripcion: String,
+    fecha: String,
+    monto: Number,
+    onClic: () -> Unit
+){
+    Card(
+        modifier = Modifier
+            .padding(20.dp),
+        shape = RoundedCornerShape(
+            topEnd = 10.dp, topStart = 10.dp, bottomEnd = 10.dp, bottomStart = 10.dp),
+        colors = CardDefaults.cardColors(containerColor = colorResource(id = R.color.grisBajito))
+
+    ){
+        Column (modifier = Modifier
+            .padding(10.dp)) {
+            //CLIENTE
+            Text(text = "Abono", modifier = Modifier.fillMaxWidth(), fontSize = 20.sp)
+
+            Row (modifier = Modifier.padding(10.dp)){
+                //DESCRIPCIÓN
+                Text(text = "Descripción: "+descripcion, Modifier.padding(10.dp))
+                //FECHA
+                Text(text = "Fecha: "+fecha, Modifier.padding(10.dp))
+                //MONTO
+                Text(text = "Monto: "+monto.toString(), Modifier.padding(10.dp))
+
+
+                Spacer(modifier = Modifier.weight(1f))
+                //ICONO ELIMINAR
+                IconButton(onClick = { }) {
+                    Icon(painter = painterResource(id = R.drawable.trash_icon),
+                        contentDescription = "detalleIcon")
+                }
+            }
+        }
+    }
+}
