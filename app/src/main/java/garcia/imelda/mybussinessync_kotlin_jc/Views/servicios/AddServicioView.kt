af4457e96de217398d51450e9af45d5b14ab1e72
DamianMlM <leonel.mariscal5040@alumnos.udg.mx>
2024-11-25 00:26:37 -0600

    Se corrige para el usuario el campo de contacto que diga telefono y solo permite teclado númerico

--- app/src/main/java/garcia/imelda/mybussinessync_kotlin_jc/Views/servicios/AddServicioView.kt
+++ app/src/main/java/garcia/imelda/mybussinessync_kotlin_jc/Views/servicios/AddServicioView.kt
@@ -100,7 +100,8 @@ fun AddServicioView(navController: NavController, serviciosVM: ServiciosViewMode
             OutlinedTextField(
                 value = numero,
                 onValueChange = {numero = it},
-                label = { Text(text = "Contacto del cliente")},
+                label = { Text(text = "Telefono del cliente")},
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(10.dp))
